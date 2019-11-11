69095288cd5d1e2c8b9fd9df5a6a28a80d7669ab
Burak Yavuzkılıç <burakyvzklcc@gmail.com>
2019-11-11 17:37:31 +0300

    Add files via upload

---
+++ c-codes-in-clion/deneme/cmake-build-debug/CMakeFiles/deneme.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/deneme.dir/main.c.o"
+  "deneme.pdb"
+  "deneme"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/deneme.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ c-codes-in-clion/deneme/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+int t,s1,s2;
+printf("1. sayi:");
+scanf("%d",&s1);
+printf("Ikinci sayi: ");
+scanf("%d",&s2);
+
+t=s1 +s2;
+printf("toplammmm: %d",t);
+
+    return 0;
+}
